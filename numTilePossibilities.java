153ccaa3be383322853fbc2ae4153474a8e70cc4
kyleyiyang <kyleyiyang@gmail.com>
2021-07-18 10:20:15 -0400

    Update numTilePossibilities.java

--- numTilePossibilities.java
+++ numTilePossibilities.java
@@ -20,3 +20,25 @@ class Solution {
         }
     }
 }
+
+***************
+
+class Solution {
+    public int numTilePossibilities(String tiles) {
+        boolean[] visited=new boolean[tiles.length()];
+        char[] chars = tiles.toCharArray();
+        Arrays.sort(chars);
+        return backtrack(chars,0,visited)-1;
+    }
+    public int backtrack(char[] chars,int start,boolean[] visited) {
+        if (start>chars.length) return 0;
+        int count=1;
+        for (int i=0;i<chars.length;i++) {
+            if(visited[i] || i> 0 && chars[i] == chars[i - 1] && !visited[i - 1]) continue;
+            visited[i]=true;
+            count+=backtrack(chars,i+1,visited);
+            visited[i]=false;
+        }
+        return count;
+    }
+}
